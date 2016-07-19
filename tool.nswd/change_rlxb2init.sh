c81c21cb135fb57b56bcb4341b46d4bf33e334a5
atrtnkw <atrtnkw@gmail.com>
2016-07-19 14:26:30 +0900

    move make_nswd.sh

--- tool.nswd/change_rlxb2init.sh
+++ tool.nswd/change_rlxb2init.sh
@@ -1,13 +1,11 @@
-if test $# -ne 1
+if test $# -ne 2
 then
-    echo "sh $0 <idir>"
+    echo "sh $0 <idir> <ofile>"
     exit
 fi
 
 idir=$1
+ofile=$2
 
-awk -f ~/git-sph/tool.hgas/change_t2t.awk "$idir"/final.dat \
-    | awk -f ~/git-sph/tool.hgas/zero_alphu.awk \
-    > "$idir"/bns-wd0.6.atc0.data
-awk -f ~/git-sph/tool.hgas/change_t2t_bhns.awk "$idir"/bhns_final.dat \
-    > "$idir"/bns-wd0.6.atc0.bhns
+awk -f ~/git-sph/tool.hgas/change_t2t.awk "$idir"/final.dat > "$ofile".data
+awk -f ~/git-sph/tool.hgas/change_t2t_bhns.awk "$idir"/bhns_final.dat > "$ofile".bhns
--- tool.nswd/make_nswd.sh
+++ tool.nswd/make_nswd.sh
@@ -1,13 +1,14 @@
-if test $# -ne 4
+if test $# -ne 5
 then
-    echo "sh $0 <NSmass[Msun]> <smallerWD> <sep/Rlobe> <ofile>"
+    echo "sh $0 <NSmass[Msun]> <snapWD> <rWD> <sep/Rlobe> <ofile>"
     exit
 fi
 
 nsmas=$1
 small=$2
-clobe=$3
-ofile=$4
+rc=$3
+clobe=$4
+ofile=$5
 bneps=1e6
 gravc=0.000000066738480
 
@@ -16,7 +17,7 @@ m2=`awk 'BEGIN{mtot=0.0;}{mtot+=$3}END{printf("%lf\n", mtot);}' $small`
 
 qq=`echo "scale=10; $m2 / $m1" | bc`
 
-rc=`awk '{print sqrt($4**2+$5**2+$6**2)}' $small | sort -g | tail -n1`
+#rc=`awk '{print sqrt($4**2+$5**2+$6**2)}' $small | sort -g | tail -n1`
 
 echo "0" > dummy
 
@@ -36,6 +37,10 @@ echo "Mass ratio: $qq" >&2
 echo "Secondary radius: $rc" >&2
 echo "Binary separation: $a0" >&2
 echo "Relative velocity: $v0" >&2
+echo "Mass ratio: $qq" > "$ofile".log
+echo "Secondary radius: $rc" >> "$ofile".log
+echo "Binary separation: $a0" >> "$ofile".log
+echo "Relative velocity: $v0" >> "$ofile".log
 
 awk '{printf("%10d %2d %+e %+e %+e %+e %+e %+e %+e %+e\n", 0, 0, mns, dr, 0., 0., 0., dv, 0., eps);}' mns=$m1 dr=$r1 dv=$v1 eps=$bneps dummy > "$ofile".bhns
 awk '{printf("%10d %2d %+e %+e %+e %+e %+e %+e %+e %+e %+e %+e %+e %+.3e %+.3e %+.3e %+.3e %+.3e %+.3e %+.3e %+.3e %+.3e %+.3e %+.3e %+.3e %+.3e\n", $1+di, 1, $3, $4+dr, $5, $6, $7, $8+dv, $9, $13, $14, $15, $17, $32, $33, $34, $35, $36, $37, $38, $39, $40, $41, $42, $43, $44);}' di=$n1 dr=$r2 dv=$v2 $small > "$ofile".data
