7a3befb6dd0a5fc9bf24ce4ec4da5339add6a14c
atrtnkw <atrtnkw@gmail.com>
2016-08-02 08:46:15 +0900

    add puttogether_time.sh

---
+++ tool.hgas/puttogether_time.sh
@@ -0,0 +1,29 @@
+if test $# -ne 3
+then
+    echo "sh $0 <basedir> <txx> <tyy>" >&2
+    exit
+fi
+
+bdir=$1
+dbgn=$2
+dend=$3
+
+prevt=-1
+
+if test -e tmp.time
+then
+    rm -f tmp.time
+fi
+
+for ndir in $(seq -f "%02g" $dbgn 1 $dend)
+do
+    idir="$bdir"/t"$ndir"
+    echo "start $idir" >&2
+    ifile="$idir"/time.log
+    awk '{if($1=="#"||$3>prevt) print $0;}' prevt=$prevt $ifile >> tmp.time
+    prevt=`tail -n1 tmp.time | awk '{print $3}'`
+done
+
+cat tmp.time
+
+rm -f tmp.time
