af0e60869b283074e1ce38675e41a3a8928f508c
atrtnkw <atrtnkw@gmail.com>
2016-08-30 09:48:01 +0900

    revise tool.nswd/extract_deleted.sh

--- tool.nswd/extract_deleted.sh
+++ tool.nswd/extract_deleted.sh
@@ -1,5 +1,7 @@
 idir=.
 
-awk '{if($1=="###"&&NF>40) print $0;}' "$idir"/t*/debug_p* \
-    | sort -n -k 2 \
-    | awk 'BEGIN{pid=-1;}{if($2!=pid) print $0; pid=$2;}'
+awk '{if($1=="###"&&NF>40){$1="";print $0;}}' "$idir"/t*/debug_p* \
+    | sort -n \
+    | awk 'BEGIN{pid=-1;}{if($1!=pid) print $0; pid=$1;}'
+#    | sort -n -k 2 \
+#    | awk 'BEGIN{pid=-1;}{if($2!=pid) print $0; pid=$2;}'
