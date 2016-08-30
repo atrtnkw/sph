c171fc1bd9b5c314c25c065bcd85694a93177312
atrtnkw <atrtnkw@gmail.com>
2016-08-30 10:01:31 +0900

    revise tool.delete/delete_hexa.sh

--- tool.delete/delete_hexa.sh
+++ tool.delete/delete_hexa.sh
@@ -4,40 +4,40 @@ then
     exit
 fi
 
-idir=$1
+tdir=$1
 dsnp=$2
 
-for time in $(seq -f "%04g" 0 $dsnp 9999)
+for idir in $tdir
 do
-    file="$idir"/t"$time"_p000000.hexa
-    if test -e $file
-    then
-        tbgn=$time
-        break
-    fi
+    echo "start directory $idir"
+    for time in $(seq -f "%04g" 0 $dsnp 9999)
+    do
+        file="$idir"/t"$time"_p000000.hexa
+        if test -e $file
+        then
+            tbgn=$time
+            break
+        fi
+    done
+    for time in $(seq -f "%04g" $tbgn $dsnp 9999)
+    do
+        file="$idir"/t"$time"_p000000.hexa
+        if test -e $file
+        then
+            tend=$time
+        fi
+        if ! test -e $file
+        then
+            break
+        fi
+    done
+    echo "tbgn: $tbgn tend: $tend" 1>&2
+    for time in $(seq -f "%04g" $tbgn $dsnp $tend)
+    do
+        if test $time -ne $tbgn -a  $time -ne $tend
+        then
+            rm -f "$idir"/t"$time"_p*.hexa
+        fi
+    done
+    touch "$idir"/deleted.log
 done
-
-for time in $(seq -f "%04g" $tbgn $dsnp 9999)
-do
-    file="$idir"/t"$time"_p000000.hexa
-    if test -e $file
-    then
-        tend=$time
-    fi
-    if ! test -e $file
-    then
-        break
-    fi
-done
-
-echo "tbgn: $tbgn tend: $tend" 1>&2
-
-for time in $(seq -f "%04g" $tbgn $dsnp $tend)
-do
-    if test $time -ne $tbgn -a  $time -ne $tend
-    then
-        rm -f "$idir"/t"$time"_p*.hexa
-    fi
-done
-
-touch "$idir"/deleted.log
