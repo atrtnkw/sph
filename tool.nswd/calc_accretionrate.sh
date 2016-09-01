3182e18c37e543c189490c9572d710586aef6ed1
tanikawa <tanikawa@localhost.localdomain>
2016-09-01 18:57:24 +0900

    add tool.nswd/calc_accretionrate.sh

---
+++ tool.nswd/calc_accretionrate.sh
@@ -0,0 +1,10 @@
+if test $# -ne 2
+then
+    echo "sh $0 <ifile> <n/1msun>" >&2
+    exit
+fi
+
+ifile=$1
+nmsun=$2
+
+awk '{if(NR==1){pn=$2;}else{printf("%4d %+e\n", $1, (pn-$2)/nsun);} pn=$2;}' nsun=$nmsun $ifile
