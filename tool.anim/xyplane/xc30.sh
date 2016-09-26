20f7e1a56b82b1e774a738821878750a2d63c713
atrtnkw <atrtnkw@gmail.com>
2016-09-26 11:42:39 +0900

    update something

--- tool.anim/xyplane/xc30.sh
+++ tool.anim/xyplane/xc30.sh
@@ -5,13 +5,13 @@
 
 cd $PBS_O_WORKDIR
 
-echo "/work/tanikwat/git-sph/nswd/data/r128k/run.bns-wd1.0/unfy"  > input.list
-echo "/work/tanikwat/git-sph/nswd/data/r128k/run.bns-wd1.0/anim/xy03e09" >> input.list
-echo "1031 1500" >> input.list
-echo "-3e9 -3e9 0." >> input.list
-echo "6e9 256" >> input.list
-#echo "-5e9 -5e9 0." >> input.list
-#echo "10e9 256" >> input.list
+echo "/work/tanikwat/git-sph/nswd/data/r128k/run.bns-wd0.6/unfy"  > input.list
+echo "/work/tanikwat/git-sph/nswd/data/r128k/run.bns-wd0.6/anim/xy03e09" >> input.list
+echo "0 3000" >> input.list
+#echo "-3e9 -3e9 0." >> input.list
+#echo "6e9 256" >> input.list
+echo "-5e9 -5e9 0." >> input.list
+echo "10e9 256" >> input.list
 
 aprun -n 1 -d 1 ./run input.list
 
--- tool.nswd/extract_deleted.sh
+++ tool.nswd/extract_deleted.sh
@@ -6,8 +6,9 @@ fi
 
 idir=$1
 
-awk '{if($1=="###"&&NF>40){$1="";print $0;}}' "$idir"/t*/debug_p* \
-    | sort -n \
-    | awk 'BEGIN{pid=-1;}{if($1!=pid) print $0; pid=$1;}'
+awk '{if($1=="###"&&NF>40){$1="";print $0;}}' "$idir"/t*/debug_p* > tmp.del
+sort -n tmp.del | awk 'BEGIN{pid=-1;}{if($1!=pid) print $0; pid=$1;}' 
+rm -r tmp.del
+
 #    | sort -n -k 2 \
 #    | awk 'BEGIN{pid=-1;}{if($2!=pid) print $0; pid=$2;}'
