e81c1065fb209dc35aa765ef679f31e725ba0b5f
atrtnkw <atrtnkw@gmail.com>
2017-05-14 11:51:40 +0900

    add tool.ddet

---
+++ tool.ddet/many_slice_dy.sh
@@ -0,0 +1,30 @@
+if test -ne 5
+then
+    echo "sh $0 <dy> <nprc> <tbgn> <tend> <dtsp>"
+    exit
+fi
+
+dy=$1
+nprc=`printf "%06d" $2`
+tbgn=$3
+tend=$4
+dtsp=$5
+
+for time in $(seq -f "%04g" $tbgn $dtsp $tend)
+do
+    for dnum in $(seq -f "%02g" 0 1 99)
+    do
+        if test -e sph_t"$time".dat.dy
+        then
+            continue
+        fi
+        if ! test -e t"$dnum"/sph_t"$time"_p"$nprc"_i000000.dat
+        then
+            echo "Not found t"$dnum"/sph_t"$time".dat"
+            continue
+        fi
+        echo "Process $time ..." >&2
+        awk -f ~/git-sph/tool.hgas/slice.awk dy="$dy" t"$dnum"/sph_t"$time"*.dat \
+            > sph_t"$time".dat.dy
+    done
+done
--- tool.exwd/make3d/xc30_mk3d.sh
+++ tool.exwd/make3d/xc30_mk3d.sh
@@ -5,26 +5,30 @@
 
 NPARALLEL=1
 NPROCESS=$NPARALLEL
-ifile=../r001m/rlx2_s1.00/s1.00_h060-060.data
-ofile=../r001m/init/s1.00_h060-060.data
+ifile=../r001m/rlx2_s1.05/s1.05_h050-100.data
+tfile=../r001m/run.s1.05_h050-100/init/s1.05_h050-100
 iflag=0
 size=1e8
 #size=5e7
 #size=3e7
 spotx=0.0
 spoty=0.0
-spotz=4.2e8
+#spotz=4.2e8 #s1.00_h100-100
+#spotz=3.9e8 #s1.00_h150-100
+#spotz=3.7e8 #s1.00_h200-100
+spotz=3.6e8  #s1.05_h050-100
 
 cd $PBS_O_WORKDIR
 
 export OMP_NUM_THREADS=`echo "$NPARALLEL / $NPROCESS" | bc`
 
 nptcl=`wc -l $ifile | awk '{print $1}'`
-echo $nptcl  > input.list
-echo $ifile >> input.list
-echo $ofile >> input.list
-echo $iflag >> input.list
-echo $size  >> input.list
+echo $nptcl       > input.list
+echo $ifile      >> input.list
+echo $tfile.data >> input.list
+echo $iflag      >> input.list
+echo $size       >> input.list
 echo $spotx $spoty $spotz >> input.list
+cp input.list $tfile.log
 
 aprun -n $NPROCESS -d $OMP_NUM_THREADS ./run input.list
