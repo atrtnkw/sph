ef4ba1e330f182ff9212da3c4750058952b41e96
atrtnkw <atrtnkw@gmail.com>
2017-07-04 14:25:38 +0900

    add tool.ddet/sort_by_id.sh

---
+++ tool.ddet/sort_by_id.sh
@@ -0,0 +1,20 @@
+idir=t01
+
+if ! test -e list
+then
+    mkdir list
+fi
+
+awk '{printf("%16.10f %+e %+e\n", $1, $3, $4) > sprintf("list/co.dat.%010d", $2);}' \
+    "$idir"/debug_p*.log
+
+exit
+
+lfile=`ls list/`
+for file in $lfile
+do
+    sort -g $lfile > tmp.sort
+    mv tmp.sort > $lfile
+done
+
+rm -f tmp.sort
