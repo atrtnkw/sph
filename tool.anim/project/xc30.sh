5d36775e651c63975b420b52462dd5e52ba814bc
atrtnkw <atrtnkw@gmail.com>
2018-02-27 07:52:03 +0900

    revise tool.anim/project/Makefile

--- tool.anim/project/xc30.sh
+++ tool.anim/project/xc30.sh
@@ -9,7 +9,8 @@ OMP_NUM_THREADS=1
 
 cd $PBS_O_WORKDIR
 
-echo "/work/tanikwat/git-sph/ddet/data/r001m/run.s1.00_h090-090/unfy"  > input.list
+#echo "/work/tanikwat/git-sph/ddet/data/r001m/run.s1.00_h090-090/unfy"  > input.list
+echo "/work/tanikwat/git-sph/ddet/data/r001m/run.s1.00_h090-090/"  > input.list
 echo "/work/tanikwat/git-sph/ddet/data/r001m/run.s1.00_h090-090/anim02e09" >> input.list
 echo "80 128" >> input.list
 echo "0 1 0 0" >> input.list
