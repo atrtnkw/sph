cef518631033e2030f0459e5f1f7e2113116eae0
atrtnkw <atrtnkw@gmail.com>
2018-03-22 19:54:57 +0900

    revise tool.exwd

--- tool.anim/project/ofp.sh
+++ tool.anim/project/ofp.sh
@@ -5,13 +5,15 @@
 #PJM -g xg17i056
 #PJM -N project
 #PJM -j
-#PJM -L node=96
-#PJM --mpi proc=6144
+#PJM -L node=24
+#PJM --mpi proc=1536
 #PJM --omp thread=1
 
-echo "/work/1/xg17i022/x10049/git-sph/imbh/data/r064m/run.hewd0.45_bh3e2_b06.00/"  > input.list
-echo "/work/1/xg17i022/x10049/git-sph/imbh/data/r064m/run.hewd0.45_bh3e2_b06.00/anim10e09/" >> input.list
-echo "99 100" >> input.list
+#echo "/work/1/xg17i022/x10049/git-sph/imbh/data/r064m/run.hewd0.45_bh3e2_b06.00/"  > input.list
+echo "/work/1/xg17i022/x10049/git-sph/imbh/"  > input.list
+#echo "/work/1/xg17i022/x10049/git-sph/imbh/data/r064m/run.hewd0.45_bh3e2_b06.00/anim10e09/" >> input.list
+echo "/work/1/xg17i022/x10049/git-sph/imbh/anim10e09/" >> input.list
+echo "92 100" >> input.list
 echo "0 0 1 0" >> input.list
 echo "0 0" >> input.list
 echo "10e9" >> input.list
