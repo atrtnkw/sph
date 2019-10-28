b3e4cf4a4685b7f4c5d1ef9a137ea89bec29a9e1
atrtnkw <atrtnkw@gmail.com>
2019-10-28 09:33:47 +0900

    tool.imbh/plot1d

---
+++ tool.imbh/plot1d.sh
@@ -0,0 +1,32 @@
+if test $# -ne 3
+then
+    echo "sh $0 <tbgn> <tend> <dtsp>" >&2
+    exit
+fi
+
+tbgn=$1
+tend=$2
+dtsp=$3
+
+alias python3='~/work/yt-conda/bin/python3'
+#python3 ~/git-sph/tool.cdet/plot1d.py $tbgn $tend $dtsp
+python3 plot1d.py $tbgn $tend $dtsp
+
+for time in $(seq $tbgn $dtsp $tend)
+do
+    if test $time -eq $tend
+    then
+        continue
+    fi
+    time=`printf "%04d" $time`
+
+#    awk '{for(i=0;i<20;i++) printf(" %+e", $(2*i+1));printf("\n");}' mesh_t"$time".dat > tmp.dat
+    awk '{for(i=0;i<17;i++) printf(" %+e", $(2*i+1));printf("\n");}' mesh_t"$time".dat > tmp.dat
+    n=`head -n1 mesh_t"$time".dat | awk '{print NF}'`
+    if test $n -eq 20
+    then
+        rm -f tmp.dat
+    else
+        mv tmp.dat mesh_t"$time".dat
+    fi
+done
