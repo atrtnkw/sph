91ce901312c7156cae20738f9938bf3ce83493df
atrtnkw <atrtnkw@gmail.com>
2019-11-11 10:54:41 +0900

    add det1dplot

---
+++ tool.tde2d/det1dplot.py
@@ -0,0 +1,28 @@
+from yt.mods import *
+import sys
+
+filetype = "det1d_hdf5_chk_"
+
+argvs = sys.argv
+argc  = len(argvs)
+if (argc != 4):
+    print ('python plot1d.py <tbgn> <tend> <dtsp>')
+    quit()
+
+tbgn = int(argvs[1])
+tend = int(argvs[2])
+dtsp = int(argvs[3])
+
+for time in range(tbgn,tend,dtsp):
+    ctime = str(time).zfill(4)
+    ifile = filetype + ctime
+    ofile = "mesh_t" + ctime + ".dat"
+
+    if (not os.path.exists(ifile)) or os.path.exists(ofile) :
+        continue
+
+    ds = load(ifile)
+    ad = ds.h.all_data()
+    obj = open(ofile, 'w')
+    for i in range(ad["temp"].size):
+        print (ad["x"][i], ad["velx"][i], ad["dens"][i], ad["temp"][i], ad["he4 "][i], ad["c12 "][i], ad["o16 "][i], ad["ne20"][i], ad["mg24"][i], ad["si28"][i], ad["s32 "][i], ad["ar36"][i], ad["ca40"][i], ad["ti44"][i], ad["cr48"][i], ad["fe52"][i], ad["ni56"][i], file=obj)
--- tool.tde2d/det1dplot.sh
+++ tool.tde2d/det1dplot.sh
@@ -8,10 +8,10 @@ tbgn=$1
 tend=$2
 dtsp=$3
 tdir=`dirname $0`
-nfmesh=6
+nfmesh=17
 
 alias python3='~/work/yt-conda/bin/python3'
-python3 "$tdir"/small2d.py $tbgn $tend $dtsp
+python3 "$tdir"/det1dplot.py $tbgn $tend $dtsp
 
 for time in $(seq $tbgn $dtsp $tend)
 do
@@ -21,25 +21,25 @@ do
     fi
     time=`printf "%04d" $time`
 
-    if ! test -e small_t"$time".dat
+    if ! test -e mesh_t"$time".dat
     then
-        echo "small_t$time.dat is not found." >&2
+        echo "mesh_t$time.dat is not found." >&2
         continue
     fi
 
-    nf=`awk '{if(NR==1) print NF}' small_t"$time".dat`
+    nf=`awk '{if(NR==1) print NF}' mesh_t"$time".dat`
     if test $nf = $nfmesh
     then
-        echo "small_t$time.dat already exists." >&2
+        echo "mesh_t$time.dat already exists." >&2
         continue
     fi
 
-    awk '{printf("%+e %+e %+e %+e %+e %+e\n", $1, $3, 0., 0., $5, $7);}' small_t"$time".dat > tmp.dat
-    n=`head -n1 small_t"$time".dat | awk '{print NF}'`
+    awk '{for(i=0;i<nf;i++){printf(" %+e", $(2*i+1));}printf("\n");}' nf=$nfmesh mesh_t"$time".dat > tmp.dat
+    n=`head -n1 mesh_t"$time".dat | awk '{print NF}'`
     if test $n -eq $nfmesh
     then
         rm -f tmp.dat
     else
-        mv tmp.dat small_t"$time".dat
+        mv tmp.dat mesh_t"$time".dat
     fi
 done
